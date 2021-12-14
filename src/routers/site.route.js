46205e8c9d34759e3eafbf9c57c6bdb7b11bb9d7
hoang <hoahuy2606@gmail.com>
2021-12-14 22:31:07 +0700

    update site thành Site

--- src/routers/site.route.js
+++ src/routers/site.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const siteController = require('../app/controllers/siteController');
+const siteController = require('../app/controllers/SiteController');
 
 router.get('/', siteController.index);
 
